037467212e3598a55d710ec4eace49f1ff595544
Prabhakar <pjonnavithula@salesforce.com>
2023-02-13 12:00:20 +0530

    adding copy_main.tf

---
+++ copy_main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAYI6GTPFSG2GWQLH2"
+  secret_key = "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"
+}
+
+resource "aws_instance" "my_first_terraform_ec2" {
+    ami = "ami-0fe472d8a85bc7b0e"
+    instance_type = "t2.micro"  
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "static-jps-webpage"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://static-jps-webpage"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
